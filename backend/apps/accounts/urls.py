c64cc538cb5bd01e3d81e1968c57a6f008d9e23a
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-05-24 23:02:04 +0200

    fix: add profile endpoint

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,9 +1,10 @@
 # accounts/urls.py
 
 from django.urls import path
-from .views import UserRegistrationAPIView, UserLoginAPIView
+from .views import UserRegistrationAPIView, UserLoginAPIView, UserProfileAPIView
 
 urlpatterns = [
     path('signup/', UserRegistrationAPIView.as_view(), name='user-registration'),
     path('login/', UserLoginAPIView.as_view(), name='user-login'),
+    path('profile/', UserProfileAPIView.as_view(), name='user-profile'),
 ]
