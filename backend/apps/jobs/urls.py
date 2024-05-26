4977135c3be72bfc4fafadda92b37c28097fd758
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-05-26 03:35:09 +0200

    feat: add toggle job-status and  endpoint

--- backend/apps/jobs/urls.py
+++ backend/apps/jobs/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import JobViewSet
+from .views import SavedJobListView
 
 router = DefaultRouter()
-router.register(r'jobs', JobViewSet)
+router.register(r'jobs', JobViewSet, basename='job')
 
 urlpatterns = [
     path('', include(router.urls)),
+     path('saved-jobs/', SavedJobListView.as_view(), name='saved-jobs-list'),
 ]
--- backend/apps/jobs/views.py
+++ backend/apps/jobs/views.py
@@ -1,8 +1,8 @@
-#jobs/views.py
-
-from rest_framework import viewsets, permissions
+# jobs/views.py
+from rest_framework import viewsets, permissions, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Job
 from .serializers import JobSerializer
 
@@ -26,3 +26,18 @@ class JobViewSet(viewsets.ModelViewSet):
             return Response({'status': 'status updated'})
         else:
             return Response({'error': 'Invalid status'}, status=400)
+
+    @action(detail=True, methods=['post'])
+    def toggle_save(self, request, pk=None):
+        job = self.get_object()
+        job.saved = not job.saved
+        job.save()
+        return Response({'status': 'saved' if job.saved else 'unsaved'})
+
+class SavedJobListView(generics.ListAPIView):
+    serializer_class = JobSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Job.objects.filter(user=user, status='saved')
